48c527ea4e382afe2e92982a61ecefd38c3f2ae3
James Neff <james.m.neff@gmail.com>
2022-05-27 13:55:06 -0400

    Update links to point to images in folder

--- server.js
+++ server.js
@@ -17,40 +17,40 @@ const server = http.createServer((req, res) => {
       let cartoonImage = null;
       switch(params['month'].toLowerCase()) {
         case 'jan':
-          cartoonImage = "https://static.wikia.nocookie.net/logocomics/images/1/10/Super_friends_logo.gif";
+          cartoonImage = "images/super_friends.jpg";
           break;
         case 'feb':
-          cartoonImage = "https://1000logos.net/wp-content/uploads/2018/11/Ninja-Turtles-logo.jpg";
+          cartoonImage = "images/ninja_turtles.jpg";
           break;
         case 'mar':
-          cartoonImage = "https://www.cartoonbucket.com/wp-content/uploads/2017/03/Image-of-Darkwing-Duck.png";
+          cartoonImage = "images/darkwing_duck.png";
           break;
         case 'apr':
-          cartoonImage = "https://m.media-amazon.com/images/M/MV5BODgyYzRhNzAtMTJlNC00OTM0LWI3M2ItNzZjYzYxZmJiOWIyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDUxNjc5NjY@._V1_.jpg"
+          cartoonImage = "images/he-man.jpg"
           break;
         case 'may':
-          cartoonImage = "https://upload.wikimedia.org/wikipedia/en/8/8a/Animaniacs.png"
+          cartoonImage = "images/animaniacs.png"
           break;
         case 'jun':
-          cartoonImage = "https://static.wikia.nocookie.net/logopedia/images/4/42/CatDog.svg/revision/latest?cb=20200719151641"
+          cartoonImage = "images/cat_dog.png"
           break;
         case 'jul':
-          cartoonImage = "https://sbocmedia.com/wp-content/uploads/2016/07/The-Tick-Logo.jpg"
+          cartoonImage = "images/the_tick.jpg"
           break;
         case 'aug':
-          cartoonImage = "https://static.wikia.nocookie.net/international-entertainment-project/images/8/8d/Garfield_and_Friends_-_logo_%28English%29.png/revision/latest?cb=20181225223332"
+          cartoonImage = "images/garfield.webp"
           break;
         case 'sep':
-          cartoonImage = "https://upload.wikimedia.org/wikipedia/en/a/a6/Pinky_and_The_Brain_%28logo%29.png"
+          cartoonImage = "images/pinky_and_the_brain.png"
           break;
         case 'oct':
-          cartoonImage = "https://static.wikia.nocookie.net/logo-timeline/images/8/85/TJ_LOGO.jpg/revision/latest/scale-to-width-down/220?cb=20160320180558";
+          cartoonImage = "images/tom_and_jerry.webp";
           break;
         case 'nov':
-          cartoonImage = "https://www.3djoes.com/uploads/1/3/3/9/13396852/gijoe-1982-1985-2d-logo-revised_orig.jpg"
+          cartoonImage = "images/gi_joe.jpg"
           break;
         case 'dec':
-          cartoonImage = "images/december.jpg";
+          cartoonImage = "images/thundercats.jpg";
           break;
         default : 
           cartoonImage = 'images/gotLost.webp';
@@ -65,6 +65,7 @@ const server = http.createServer((req, res) => {
       res.end(JSON.stringify(objToJson));
     }
   }
+  
   //// HANDLE PAGE NAVIGATION ///////////////////////////
   let contentType;
   let extName = path.extname(req.url);
@@ -127,95 +128,6 @@ const server = http.createServer((req, res) => {
       res.end(data, 'utf-8');
     }
   })
-
-
-
-  // if (page == '/') {
-  //   fs.readFile('index.html', function(err, data) {
-  //     res.writeHead(200, {'Content-Type': 'text/html'});
-  //     res.write(data);
-  //     res.end();
-  //   });
-  // }
-  // else if (page == '/api') {
-  //   console.log('params', params)
-
-  //   if('month' in params){
-      
-  //     let cartoonImage = null;
-
-  //     switch(params['month'].toLowerCase()) {
-  //       case 'jan':
-  //         cartoonImage = "https://static.wikia.nocookie.net/logocomics/images/1/10/Super_friends_logo.gif";
-  //         break;
-  //       case 'feb':
-  //         cartoonImage = "https://1000logos.net/wp-content/uploads/2018/11/Ninja-Turtles-logo.jpg";
-  //         break;
-  //       case 'mar':
-  //         cartoonImage = "https://www.cartoonbucket.com/wp-content/uploads/2017/03/Image-of-Darkwing-Duck.png";
-  //         break;
-  //       case 'apr':
-  //         cartoonImage = "https://m.media-amazon.com/images/M/MV5BODgyYzRhNzAtMTJlNC00OTM0LWI3M2ItNzZjYzYxZmJiOWIyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDUxNjc5NjY@._V1_.jpg"
-  //         break;
-  //       case 'may':
-  //         cartoonImage = "https://upload.wikimedia.org/wikipedia/en/8/8a/Animaniacs.png"
-  //         break;
-  //       case 'jun':
-  //         cartoonImage = "https://static.wikia.nocookie.net/logopedia/images/4/42/CatDog.svg/revision/latest?cb=20200719151641"
-  //         break;
-  //       case 'jul':
-  //         cartoonImage = "https://sbocmedia.com/wp-content/uploads/2016/07/The-Tick-Logo.jpg"
-  //         break;
-  //       case 'aug':
-  //         cartoonImage = "https://static.wikia.nocookie.net/international-entertainment-project/images/8/8d/Garfield_and_Friends_-_logo_%28English%29.png/revision/latest?cb=20181225223332"
-  //         break;
-  //       case 'sep':
-  //         cartoonImage = "https://upload.wikimedia.org/wikipedia/en/a/a6/Pinky_and_The_Brain_%28logo%29.png"
-  //         break;
-  //       case 'oct':
-  //         cartoonImage = "https://static.wikia.nocookie.net/logo-timeline/images/8/85/TJ_LOGO.jpg/revision/latest/scale-to-width-down/220?cb=20160320180558";
-  //         break;
-  //       case 'nov':
-  //         cartoonImage = "https://www.3djoes.com/uploads/1/3/3/9/13396852/gijoe-1982-1985-2d-logo-revised_orig.jpg"
-  //         break;
-  //       case 'dec':
-  //         cartoonImage = "public/images/december.jpg";
-  //         break;
-  //       default : 
-  //         cartoonImage = 'public/images/gotLost.webp';
-
-  //     }
-
-  //     res.writeHead(200, {'Content-Type': 'application/json'});
-  //     const objToJson = {
-  //       month: params['month'],
-  //       cartoon: cartoonImage,
-  //     }
-  //     res.end(JSON.stringify(objToJson));
-  //   }
-  // }
-  // if (page == '/public/css/style.css'){
-  //   fs.readFile('public/css/style.css', function(err, data) {
-  //     res.write(data);
-  //     res.end();
-  //   });
-  // }else if (page == '/public/js/main.js'){
-  //   fs.readFile('public/js/main.js', function(err, data) {
-  //     res.writeHead(200, {'Content-Type': 'text/javascript'});
-  //     res.write(data);
-  //     res.end();
-  //   });
-  // }else{
-    // fs.readFile('404.html', function(err, data) {
-    //   if (err) {
-    //       console.log('Something went wrong...');
-    //       console.dir(err);
-    //       return;
-    //   }
-    //   res.write(data);
-    //   res.end();
-    // });
-  // }
 });
 
 //setup listen and allow port assignment.
