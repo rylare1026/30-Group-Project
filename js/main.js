efba66a3a87ad0077276b9549db44ebb2bf309fa
rylare1026 <rylare1026@gmail.com>
2022-05-26 20:22:18 -0500

    Switch Case Working

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 document.querySelector('#clickMe').addEventListener('click', makeReq)
 
 async function makeReq(){
-  const month = document.querySelector("#month").value.toLowerCase();
+  const month = document.querySelector("#months").value.toLowerCase();
   const res = await fetch(`/api?month=${month}`)
   const data = await res.json()
 
   console.log(data);
   // Displays month back to user
-  document.querySelector("#month").textContent = month.toUpperCase()
-  // Changes image source
+  document.querySelector("#monthText").textContent = month.toUpperCase()
+  // Changes image source to image for current month
   document.querySelector("#cartoon").src = data.cartoon
 }
